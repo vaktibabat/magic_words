26c051dbf841ddbadeda357e0f49113bbf26dfc7
sag0li <yoray.herzberg@gmail.com>
2025-03-03 14:12:35 +0200

    Non-contextualized token embeddings when running model w/perturbations

--- utils.py
+++ utils.py
@@ -6,9 +6,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Embed a perturbed sentence -- before embedding, we append a perturbation to the sentence's token embeddings
 def embed_perturbed_text(model: SentenceTransformer, sentence: str, perturbations: torch.Tensor):    
     # Embed the sentence's tokens (shape (seq_len, h))
-    sentence_emb = model.encode(sentence, output_value="token_embeddings", convert_to_tensor=True)
+    # Initial embedding matrix of the model
+    E = model[0].auto_model.embeddings.word_embeddings.weight
+    tokens = model.tokenizer(sentence)["input_ids"][0]
+    token_embs = E[tokens]
     # Concatenate w/perturbations (shape (seq_len + m, h))
-    perturbed_sentence_emb = torch.cat([sentence_emb, perturbations.T])
+    perturbed_sentence_emb = torch.cat([token_embs, perturbations.T])
     # Create an attention mask
     attn_mask = torch.ones(1, perturbed_sentence_emb.shape[0], device=device)
     # Run the core module on the input embeddings
@@ -17,3 +20,4 @@ def embed_perturbed_text(model: SentenceTransformer, sentence: str, perturbation
     outputs = model[1].forward({"token_embeddings": outputs["last_hidden_state"], "attention_mask": attn_mask})
 
     return outputs["sentence_embedding"]
+
